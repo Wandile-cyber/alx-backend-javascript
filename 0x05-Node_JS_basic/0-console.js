6a818049e1ae0e3efff71898ab2064c5c7e73a5f
Wandile-cyber <ralphdlamini3@gmail.com>
2024-07-23 16:06:37 +0000

    node basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Wandile-cyber <https://github.com/Wandile-cyber>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
+
